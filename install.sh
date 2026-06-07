3afe47460d5faeba0295a6907ddfe6e172791275
Jonas De Decker <jonasbestnet@gmail.com>
2026-06-07 03:05:56 +0200

    Changes install script

--- install.sh
+++ install.sh
@@ -8,4 +8,20 @@ xfce4 \
 lightdm \
 kitty \
 firefox-esr \
+thunar \
+thunar-archive-plugin \
+thunar-volman \
+thunar-media-tags-plugin \
+gvfs \
+gvfs-backends \
+gvfs-fuse \
+rofi \
+network-manager-gnome \
+gnome-software \
+flatpak \
+gnome-software-plugin-flatpak \
 -y
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.libreoffice.LibreOffice
